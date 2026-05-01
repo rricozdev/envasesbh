0b34e810069930ddcbd47050e86c01a37068d359
= <=>
2026-05-01 15:52:31 -0500

    agrega las fuentes a usar en la pagina

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,54 +1,35 @@
-// import MainLayout from "@/components/layout/MainLayout";
-// import { CartProvider } from "@/context/CartContext";
-// import "./globals.css";
-// import { Inter, Source_Sans_3 } from "next/font/google";
+import "./globals.css";
+import MainLayout from "@/components/layout/MainLayout";
+import { CartProvider } from "@/context/CartContext";
+import { Inter, Source_Sans_3 } from "next/font/google";
 
-// const inter = Inter({
-//   subsets: ["latin"],
-//   variable: "--font-inter",
-// });
-// const sourceSans3 = Source_Sans_3({
-//   subsets: ["latin"],
-//   variable: "--font-source-sans-3",
-// });
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  display: "swap",
+});
 
+const sourceSans3 = Source_Sans_3({
+  subsets: ["latin"],
+  variable: "--font-source-sans-3",
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  display: "swap",
+});
+
+// TODO: EXPORTAR METADA DATA DE HOME O GENERAL
 // export const metadata = {
 //   title: "Envases BH | Soluciones en Envases PET",
 //   description: "Catálogo de envases PET de alta calidad",
 // };
 
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="es" className="scroll-smooth">
-//       <body className={`${inter.variable} ${sourceSans3.variable}`}>
-//         <CartProvider>
-//           <MainLayout>{children}</MainLayout>
-//         </CartProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-// app/layout.js
-import { CartProvider } from "@/context/CartContext";
-import MainLayout from "@/components/layout/MainLayout";
-import "./globals.css";
-import { Inter, Source_Sans_3 } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-const sourceSans3 = Source_Sans_3({
-  subsets: ["latin"],
-  variable: "--font-source-sans-3",
-});
-
-export const metadata = {
-  title: "Envases BH | Soluciones en Envases PET",
-  description: "Catálogo de envases PET de alta calidad",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className="scroll-smooth">
+    <html
+      lang="es"
+      className={`${inter.variable} ${sourceSans3.variable} h-full antialiased scroll-smooth`}
+    >
       <body className={`${inter.variable} ${sourceSans3.variable}`}>
         <CartProvider>
           <MainLayout>{children}</MainLayout>
