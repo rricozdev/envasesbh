ebb4c0ef2d13afc78479645ff0fc07e02befbd27
Shago01 <kgsanty2001@gmail.com>
2026-05-14 20:44:11 -0500

    añade animación al modal

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -12,24 +12,22 @@ export default function MainLayout({ children }) {
     <div className="flex flex-col min-h-screen w-full">
       <TopBar />
       {/* Header global de navegación (visible en todas las rutas) */}
+      <AnnouncementModal endDate="2026-06-6">
+        <div className="relative w-full max-w-140 h-70 mx-auto">
+          <Image
+            alt="Imagen promocional participación exposición"
+            className="object-contain object-center rounded-2xl"
+            fill
+            sizes="(max-width: 768px) 100vw, 420px"
+            priority
+            src="/img/promocional.webp"
+          />
+        </div>
+      </AnnouncementModal>
       <Header />
       {/* Área principal donde Next.js renderiza las páginas */}
       {/* "grow" permite que este bloque ocupe el espacio disponible */}
-      <main className="grow w-full">
-        {children}
-        <AnnouncementModal endDate="2026-06-12">
-          <div className="relative w-full max-w-140 h-70 mx-auto">
-            <Image
-              alt="Imagen promocional participación exposición"
-              className="object-contain object-center rounded-2xl"
-              fill
-              sizes="(max-width: 768px) 100vw, 420px"
-              priority
-              src="/img/promocional.webp"
-            />
-          </div>
-        </AnnouncementModal>
-      </main>
+      <main className="grow w-full">{children}</main>
       {/* Footer global del sitio */}
       <Footer />
       {/* Controlador central de paneles UI (chat, cart, overlays, etc.) */}
